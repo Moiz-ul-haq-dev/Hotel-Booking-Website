6c907eadb9eed60260fea1d1801bb2a06cfb7708
Syed-Moiz-ul-haq <78104627+Moiz-ul-haq@users.noreply.github.com>
2023-08-10 19:49:16 +0500

    ✔ Front End Updated

--- src/pages/LoginSignupPage.jsx
+++ src/pages/LoginSignupPage.jsx
@@ -4,6 +4,62 @@ import Footer from '../components/Footer';
 
 export default function LoginSignupPage() {
   const [showLogin, setShowLogin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassowrd] = useState('');
+
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassowrd(e.target.value)
+  };
+
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+    } else {
+      console.log(email, password, confirmPassword);
+      fetch('http://localhost:4000/signUp', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password, confirmPassword }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+
+    fetch('http://localhost:4000/logIn', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          
+        }
+      }
+      );
+  };
 
   const toggleLoginSignup = () => {
     setShowLogin(!showLogin);
@@ -15,11 +71,11 @@ export default function LoginSignupPage() {
       <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
         <div className={`flex flex-col items-center justify-center bg-white rounded-lg shadow-lg p-8 m-4 ${showLogin ? 'hidden' : ''}`}>
           <h1 className="text-3xl font-bold mb-4">Signup</h1>
-          <form className="flex flex-col w-full">
-            <input type="text" placeholder="Email" className="border border-gray-400 p-2 rounded-lg mb-2" />
-            <input type="password" placeholder="Password" className="border border-gray-400 p-2 rounded-lg mb-2" />
-            <input type="password" placeholder="Confirm Password" className="border border-gray-400 p-2 rounded-lg mb-2" />
-            <button className="bg-orange-400 text-white p-2 rounded-lg">Signup</button>
+          <form className="flex flex-col w-full" encType='multipart/form-data'>
+            <input type="text" placeholder="Email" className="border border-gray-400 p-2 rounded-lg mb-2" onChange={handleEmailChange} />
+            <input type="password" placeholder="Password" className="border border-gray-400 p-2 rounded-lg mb-2" onChange={handlePasswordChange} />
+            <input type="password" placeholder="Confirm Password" className="border border-gray-400 p-2 rounded-lg mb-2" onChange={handleConfirmPasswordChange} />
+            <button className="bg-orange-400 text-white p-2 rounded-lg" type='submit' onClick={handleSignUpSubmit}>Signup</button>
             <p className="mt-2">
               Already have an account?{' '}
               <span className="text-blue-500 cursor-pointer underline" onClick={toggleLoginSignup}>
@@ -31,9 +87,9 @@ export default function LoginSignupPage() {
         <div className={`flex flex-col items-center justify-center bg-white rounded-lg shadow-lg p-8 m-4 ${showLogin ? '' : 'hidden'}`}>
           <h1 className="text-3xl font-bold mb-4">Login</h1>
           <form className="flex flex-col w-full">
-            <input type="text" placeholder="Email" className="border border-gray-400 p-2 rounded-lg mb-2" />
-            <input type="password" placeholder="Password" className="border border-gray-400 p-2 rounded-lg mb-2" />
-            <button className="bg-orange-400 text-white p-2 rounded-lg">Login</button>
+            <input type="text" placeholder="Email" className="border border-gray-400 p-2 rounded-lg mb-2" onChange={handleEmailChange} />
+            <input type="password" placeholder="Password" className="border border-gray-400 p-2 rounded-lg mb-2" onChange={handlePasswordChange} />
+            <button className="bg-orange-400 text-white p-2 rounded-lg" type='submit' onClick={handleLoginSubmit}>Login</button>
             <p className="mt-2">
               Don't have an account?{' '}
               <span className="text-blue-500 cursor-pointer underline" onClick={toggleLoginSignup}>
